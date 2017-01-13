755301385b67d511cc0a916bc233408564b1bbd8
unknown <>
2017-01-13 17:49:21 +0900

    Version 1.0

---
+++ layerwindowtray.cpp
@@ -0,0 +1,199 @@
+// layerwindowtray.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "layerwindowtray.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND hWndActive;								// 현재 활성화된 창입니다.
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+BOOL CALLBACK		EnumWindowsProc(HWND hWnd, LPARAM lparam);
+BOOL CALLBACK		EnumWindowsProcBack(HWND hWnd, LPARAM lparam);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+
+	// 전역 문자열을 초기화합니다.
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_LAYERWINDOWTRAY, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 응용 프로그램 초기화를 수행합니다.
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAYERWINDOWTRAY));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다.
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lparam) {
+
+	// 부모가 바탕화면인지
+	if (GetParent(hWnd) == 0) {
+		// 최소화인지 활성화인지
+		if (!IsIconic(hWnd)) {
+			// 이름 길이가 있는지
+			if (GetWindowTextLength(hWnd) > 0) {
+				// set WS_EX_LAYERED on this Window
+				SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+				// Current active windows ALPHA 95%, else windows 70%
+				if (hWnd == hWndActive) {
+					SetLayeredWindowAttributes(hWnd, 0, (255 * 95) / 100, LWA_ALPHA);
+				}
+				else {
+					SetLayeredWindowAttributes(hWnd, 0, (255 * 70) / 100, LWA_ALPHA);
+				}
+			}
+		}
+	}
+	return TRUE;
+}
+
+BOOL CALLBACK EnumWindowsProcBack(HWND hWnd, LPARAM lparam) {
+	// 부모가 바탕화면인지
+	if (GetParent(hWnd) == 0) {
+		// 최소화인지 활성화인지
+		if (!IsIconic(hWnd)) {
+			// 이름 길이가 있는지
+			if (GetWindowTextLength(hWnd) > 0) {
+				// Remove WS_EX_LAYERED from this Window styles
+				SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | ~WS_EX_LAYERED);
+				// Ask the window and its children to repaint(???)
+				//RedrawWindow(hWnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE | RDW_FRAME | RDW_ALLCHILDREN);
+				// Current active windows ALPHA 95% 
+				SetLayeredWindowAttributes(hWnd, 0, (255 * 100) / 100, LWA_ALPHA);
+			}
+		}
+	}
+	return TRUE;
+}
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAYERWINDOWTRAY));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_LAYERWINDOWTRAY);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+	// TODO: Make this window substitute for Tray Icon with background process.
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		100, 100, 0, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적:  주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case WM_CREATE:
+		SetTimer(hWnd,						// handle to main window 
+			1,								// timer identifier 
+			500,							// 0.5-second interval 
+			(TIMERPROC)NULL);               // no timer callback 
+		break;
+	case WM_TIMER: 
+	{
+		// Get Current Active Window
+		hWndActive = GetForegroundWindow();
+
+		// All Windows Iteration
+		EnumWindows(EnumWindowsProc, NULL);
+	}
+    break;
+    case WM_DESTROY:
+		EnumWindows(EnumWindowsProcBack, NULL);
+		KillTimer(hWnd, 1);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
